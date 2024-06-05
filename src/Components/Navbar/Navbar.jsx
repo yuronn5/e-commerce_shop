735119b67dd20fb2316902898dd2a947aabb8a44
yurii bilous <yurii.b@elogic.com.ua>
2024-06-05 19:00:32 +0300

    added state for menu items, and added font

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Navbar.css';
 import logo from '../Assets/logo.png'
-import cart_icon from '../Assets/cart_icon.png'
+import cart_icon from '../Assets/cart_icon.png';
+import { useState } from 'react';
 
 const Navbar = () => {
+
+    const [menu, setMenu] = React.useState("shop");
+
   return (
     <div className='navbar'>
         <div className='nav-logo'> 
@@ -11,10 +15,10 @@ const Navbar = () => {
             <p>Shopper</p>
         </div>
         <ul className='nav-menu'>
-            <li className='nav-item'>Shop<hr/></li>
-            <li className='nav-item'>Men</li>
-            <li className='nav-item'>Women</li>
-            <li className='nav-item'>Kids</li>
+            <li onClick={() => setMenu("shop")} className='nav-item'>Shop{menu === "shop" ?  <hr/> : ""}</li>
+            <li onClick={() => setMenu("mens")} className='nav-item'>Men{menu === "mens" ?  <hr/> : ""}</li>
+            <li onClick={() => setMenu("womens")} className='nav-item'>Women{menu === "womens" ?  <hr/> : ""}</li>
+            <li onClick={() => setMenu("kids")} className='nav-item'>Kids{menu === "kids" ?  <hr/> : ""}</li>
         </ul>
         <div className='nav-login-cart'>
             <button className='login-btn'>Login</button>
