538134750b45546cdbc2033c65cd0e998077bb12
yurii bilous <yurii.b@elogic.com.ua>
2024-06-09 21:10:17 +0300

    login form container

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './Css/LoginSignup.css';
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+        <div className="loginsignup-container">
+          <h1>Sign up</h1>
+          <div className="loginsignup-fields">
+            <input type="text" placeholder='Your name' />
+            <input type="email" placeholder='Email address' />
+            <input type="password" placeholder='Password' />
+          </div>
+          <button>Continue</button>
+          <p className="loginsignup-login">
+            Alreary have an account? <span>Log in</span>
+          </p>
+          <div className="loginsignup-agree">
+            <input type="checkbox" name='' id='' />
+            <p>By continuing, I agree to the terms of use & privacy policy</p>
+          </div>
+        </div>
+    </div>
   )
 }
 
